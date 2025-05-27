444b8267156c98ee40152954dc921b4aa66e4786
Matthew McKenzie <mwmckenzie@gmail.com>
2025-05-27 06:55:03 +0200

    Add initial Vue 3 + Vite project setup with Tailwind and DaisyUI
    This commit introduces the initial structure of a Vue 3 application, bundled with Vite for fast development. It includes TailwindCSS, DaisyUI, and Pinia for styling and state management, along with essential files and configurations for quickly building scalable UIs.

---
+++ vue-tailwind-daisyui/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from "./components/navbar.vue";
+import ExampleNoInline from "./components/example-no-inline.vue";
+import { useAppConfigStore } from "./stores/appConfigStore.js";
+
+const appConfigStore = useAppConfigStore();
+</script>
+
+<template>
+  <navbar/>
+  <example-no-inline :current-theme="appConfigStore.theme"/>
+</template>
+
+
